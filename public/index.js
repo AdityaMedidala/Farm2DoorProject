6c6c72ea35073915c4cb936f7e0a24ffd8c4e9fa
AdityaMedidala <165372154+AdityaMedidala@users.noreply.github.com>
2024-10-02 14:11:28 +0530

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-// Serve static files from the 'public' directory
+// Serve files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes for different HTML pages
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
